dd66c6973cb13a4a80af4eae5ec6b71046ba6132
lukewill18 <lukewill18@gmail.com>
2017-10-30 16:08:03 -0700

    Added folders for sprites, scripts, and prefabs. Added first version of enemy following player script.

---
+++ Game/Assets/Scripts/follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    public Transform target;
+    public float speed = .01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.LookAt(target.position);
+
+        if (Vector3.Distance(transform.position, target.position) > 1)
+        {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        }
+
+    }
+}
