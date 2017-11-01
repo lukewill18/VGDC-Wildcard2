313995f4d77c801f1c2a2f2f2cbc71dd67d90e9d
mattinthecat <phoisgood1@gmail.com>
2017-11-01 11:39:06 -0700

    Pushing my scene

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+    public GameObject projectilePrefab;
+    private List<GameObject> Projectiles = new List<GameObject>();
+    private float projectileVelocity;
+
+
+	// Use this for initialization
+	void Start () {
+        projectileVelocity = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Shoot"))
+        {
+            GameObject bullet = (GameObject)Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Projectiles.Add(bullet);
+        }
+
+        for (int i = 0; i < Projectiles.Count; i++)
+        {
+            GameObject goBullet = Projectiles[i];
+            if (goBullet != null)
+            {
+                goBullet.transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * projectileVelocity);
+
+                Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint(goBullet.transform.position);
+                if (bulletScreenPos.y >= Screen.height || bulletScreenPos.y <= 0)
+                {
+                    DestroyObject(goBullet);
+                    Projectiles.Remove(goBullet);
+                }
+            }
+        }
+	}
+}
