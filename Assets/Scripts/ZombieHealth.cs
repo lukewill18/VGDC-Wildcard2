a07082f1af984d5380a061dcb2542c58ee9c197d
lukewill18 <lukewill18@gmail.com>
2017-11-08 18:58:10 -0800

    Added script to manage zombie damage, added tag to projectile prefab

---
+++ Assets/Scripts/ZombieHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : MonoBehaviour {
+
+    public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+        health = 3;
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            health--;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+            // Update is called once per frame
+            void Update () {
+		
+	}
+}
