5802cc748e7636ac2c4ded9b2092f35a4597005d
lukewill18 <lukewill18@gmail.com>
2017-11-15 17:46:39 -0800

    Bugfix for follow script

--- Assets/Scripts/follow.cs
+++ Assets/Scripts/follow.cs
@@ -9,14 +9,18 @@ public class follow : MonoBehaviour {
   
 	// Use this for initialization
 	void Start () {
-        target = GameObject.FindWithTag("Player").transform;
+        if(GameObject.FindWithTag("Player") != null)
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         //transform.LookAt(target.position);
-        if (HitByEnemy.health >=0)
+        if (target != null) //need to detect if player has died 
         {
+            Debug.Log("asdf");
             if (!Physics.Raycast(transform.position, target.position))
             {
                 transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
