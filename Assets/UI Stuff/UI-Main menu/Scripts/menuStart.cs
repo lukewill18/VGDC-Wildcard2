8d3a8a90f976fd0468114552c2c42a507b04ce66
FamousHero <33110962+FamousHero@users.noreply.github.com>
2017-11-30 16:18:34 -0800

    UI quit button fixed/color scheme change

---
+++ Assets/UI Stuff/UI-Main menu/Scripts/menuStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuStart : MonoBehaviour {
+
+    public void changemenuscene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+}
