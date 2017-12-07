dd59ff33b1bbd2bcf33c473e4b1a00cdb8912003
lukewill18 <lukewill18@gmail.com>
2017-12-06 16:05:33 -0800

    Added level switcher prefab

---
+++ Assets/Scripts/levelcontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelcontroller : MonoBehaviour
+{
+
+    public bool nextLevel = false;
+    public string nextLevelName;
+
+    void Update()
+    {
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if(allEnemies.Length == 0)
+        {
+            nextLevel = true;
+        }
+        if (nextLevel)
+        {
+            SceneManager.LoadScene(nextLevelName, LoadSceneMode.Single);
+        }
+    }
+}
