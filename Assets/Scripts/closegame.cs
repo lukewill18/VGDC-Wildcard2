443ea538aad2242679f0976db6835aedd65b8058
Isaac <isacmen@gmail.com>
2018-01-11 19:21:55 -0800

    polished it all up a bit
    restarts when you die
    added a (shitty) you win screen
    added a fifth level

---
+++ Assets/Scripts/closegame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closegame : MonoBehaviour {
+
+	int timer = 0;
+
+	void Update () {
+		timer++;
+		if (timer > 300)
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Scripts/levelrestart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelrestart : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+		if(player.Length == 0)
+		{
+			//yield return new WaitForSeconds(2);
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+}
